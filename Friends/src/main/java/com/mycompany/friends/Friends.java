9ee89a0b8ba41ddeddd129a1c18e07a7ef77130a
Paula Suarez <161689914+Pau-dna@users.noreply.github.com>
2024-09-19 10:31:40 -0500

    Add files via upload

---
+++ Friends/src/main/java/com/mycompany/friends/Friends.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.friends;
+
+/**
+ *
+ * @author paula
+ */
+public class Friends {
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+    }
+}
